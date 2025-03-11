dddce76eb5a7f0a00fae8c43f259b74118ac1f6c
Mikkel Blyme <blymemikkel@protonmail.com>
2025-03-11 10:40:40 +0100

    Rename mason -> lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls", "eslint", "tailwindcss" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.tailwindcss.setup({})
+
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+    end,
+  },
+}
