60cdf6224f5c0cd54b44ccdcc6fc0a46c506cb43
Mikkel Blyme <blymemikkel@protonmail.com>
2025-03-11 11:21:06 +0100

    Add php lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "eslint", "tailwindcss" },
+        ensure_installed = { "lua_ls", "ts_ls", "eslint", "tailwindcss", "intelephense", "phpactor" },
       })
     end,
   },
@@ -21,6 +21,8 @@ return {
       lspconfig.ts_ls.setup({})
       lspconfig.eslint.setup({})
       lspconfig.tailwindcss.setup({})
+      lspconfig.intelephense.setup({})
+      lspconfig.phpactor.setup({})
 
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
     end,
